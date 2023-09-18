fbc856aff1535435da7f77d8c6d14a4b2a985d50
Julien de Castelnau <julien.decastelnau@epfl.ch>
2023-09-18 13:22:38 +0200

    Update ZenithLib & change PID, shouldn't use 0

--- include/zenith_cfg.h
+++ include/zenith_cfg.h
@@ -5,6 +5,8 @@
 #define ZTH_MANUFACTURER "Zenith Control Labs"
 #define ZTH_PRODUCT "Phobri64"
 
+#define ZTH_PID 0x0001
+
 #define ZENITH_SERIAL_PIN 11
 
 #define ZTH_N64_REMAP_DEFAULT                                                  \
