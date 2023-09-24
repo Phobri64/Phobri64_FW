d7fe5dacd0c6c2141b2d0fe26eea48f485453525
Julien de Castelnau <julien.decastelnau@epfl.ch>
2023-09-24 12:11:00 +0200

    Update readme, zenith_lib, HW def for v1.X

--- README.md
+++ README.md
@@ -1,12 +1,3 @@
-# Phobri64 (working title)
+# Phobri64 Firmware
 
-This repository contains the software for a planned custom motherboard for Hori Mini Pad N64 controllers. 
-
-It uses code from:
-
-* [PhobGCC-SW](https://github.com/PhobGCC/PhobGCC-SW)
-* [joybus-pio](https://github.com/JonnyHaystack/joybus-pio)
-* [pico-examples](https://github.com/raspberrypi/pico-examples)
-* [Adafruit_TinyUSB_Arduino](https://github.com/adafruit/Adafruit_TinyUSB_Arduino)
-* [HayBox](https://github.com/JonnyHaystack/HayBox)
-* [polyfit](https://github.com/henryfo/polyfit)
+This repository contains the firmware for the Phobri64 controller, based off the Zenith controller library.
--- include/hw/phobri_v1_X.h
+++ include/hw/phobri_v1_X.h
@@ -5,16 +5,16 @@
 #include <hardware/i2c.h>
 
 #define JOYBUS_PIN 0
-#define BTN_A_PIN 11
-#define BTN_B_PIN 16
-#define BTN_START_PIN 25
-#define BTN_ZR_PIN 9
-#define BTN_ZL_PIN 24
-#define BTN_R_PIN 8
+#define BTN_A_PIN 7
+#define BTN_B_PIN 18
+#define BTN_START_PIN 8
+#define BTN_ZR_PIN 5
+#define BTN_ZL_PIN 28
+#define BTN_R_PIN 4
 #define BTN_L_PIN 23
 
 #define BTN_CU_PIN 2
-#define BTN_CD_PIN 10
+#define BTN_CD_PIN 6
 #define BTN_CL_PIN 1
 #define BTN_CR_PIN 3
 
@@ -24,16 +24,16 @@
 #define BTN_DR_PIN 19
 
 // TODO: change
-#define DEBUG_TX_PIN 26
-#define DEBUG_UART uart0
+#define DEBUG_TX_PIN 24
+#define DEBUG_UART uart1
 
 // TODO: change
-#define STICK_I2C_INTF i2c1
-#define STICK_I2C_SCL 18
-#define STICK_I2C_SDA 17
-#define STICK_HX_DRDY 12
-#define STICK_HY_DRDY 13
-#define STICK_ADC_DRDY_N 15
+#define STICK_I2C_INTF i2c0
+#define STICK_I2C_SCL 17
+#define STICK_I2C_SDA 16
+#define STICK_HX_DRDY 8
+#define STICK_HY_DRDY 9
+#define STICK_ADC_DRDY_N 11
 
 #define I2C_HX_ADDR 0x1E
 #define I2C_HY_ADDR 0x1C
